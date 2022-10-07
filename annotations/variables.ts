4070c5933f0880a8e59160cd7ec5614502a62449
sidhant singh <singhsidmail23@gmail.com>
2022-10-07 17:39:47 +0530

    started

---
+++ annotations/variables.ts
@@ -0,0 +1,9 @@
+let num: number = 5;
+let speed: string = "fast";
+let stat: boolean = false;
+let nothing: null = null;
+let nothingu: undefined = undefined;
+let now: Date = new Date();
+
+// type annotation for array
+let colors: string[] = ["red", "green", "blue"];
